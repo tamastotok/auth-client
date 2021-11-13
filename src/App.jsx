61ab3bafc7cf2b1f57620da01bc2ef06e18d11f4
tamastotok <tamastotok.tk@gmail.com>
2021-11-13 14:23:06 +0100

    add sass and react-router

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import devchallenges from "./assets/devchallenges.svg";
+
+function App() {
+  return (
+    <div className="auth-container">
+      <img src={devchallenges} alt="devchallenges" />
+      <h3>Join thousands of learners from around the world</h3>
+      <p>
+        Master web development by making real-life projects. There are multiple
+        paths for your to choose.
+      </p>
+    </div>
+  );
+}
+
+export default App;
